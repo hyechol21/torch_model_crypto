922849ae016d5d713bf539debf0f95ed173fc03e
hyechol21 <like3orange@gmail.com>
2025-01-11 22:50:25 +0900

    Update encrypt_model.py

--- encrypt_model.py
+++ encrypt_model.py
@@ -8,12 +8,6 @@ import random
 
 
 class TorchAES:
-    '''
-        AES 양방향 암호화
-
-        1. pth 파일 암호화한 새로운 파일 생성
-    '''
-
     def __init__(self, key_text):
         hash = SHA256.new()
         hash.update(key_text.encode('utf-8'))
@@ -46,19 +40,14 @@ class TorchAES:
                     block += b'0' * (16 - len(block) % 16)
                 ret.write(aes.encrypt(block))
 
-
-
 def generate_key(length_of_string: int) -> str:
     key = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(length_of_string))
     return key
 
 
-
 if __name__ == '__main__':
-    model_path = '/home/fourind/py36/Sorest_NX_fire detect/20211016_NX_new fire detect_v2/model/fire_model_trt.pth'
-    # model_path = '/home/fourind/fire_model_trt.pth'
+    model_path = 'fire_model_trt.pth'
     new_filename = 'test_c.pth'
-    # key = '4industry!'
     key_length = 20
 
     secret_key = generate_key(key_length)
